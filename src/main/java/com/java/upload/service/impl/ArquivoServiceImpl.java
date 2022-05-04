f6caf35ad53515ee7be61260322ba3b7fecc54a4
MaxwellJalves <maxwell880@gmail.com>
2022-05-04 02:22:07 -0300

    feacture-1002 : implementado services e lógica para salvar o arquivo local e na base de dados

---
+++ src/main/java/com/java/upload/service/impl/ArquivoServiceImpl.java
@@ -0,0 +1,56 @@
+package com.java.upload.service.impl;
+
+import com.java.upload.model.Arquivos;
+import com.java.upload.repository.ArquivosRepository;
+import com.java.upload.service.ArquivoService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+@Service
+@AllArgsConstructor
+public class ArquivoServiceImpl implements ArquivoService {
+
+    private final String DIRETORIO = "/arquivos/upload";
+
+    private ArquivosRepository arquivosRepository;
+
+    @Override
+    public void uploadLocal(MultipartFile file) {
+
+        try {
+
+            var obterBytes = file.getBytes();
+            var path = Paths.get(DIRETORIO.concat(file.getOriginalFilename()));
+            Files.write(path, obterBytes);
+
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Não foi possivel salvar o arquivo \t :" + file.getOriginalFilename());
+        }
+
+    }
+
+    @Override
+    public void uploadDataBase(MultipartFile file) {
+
+        try {
+
+            var arquivo = Arquivos.builder()
+                    .formato(file.getContentType())
+                    .arquivoData(file.getBytes())
+                    .descricao(file.getOriginalFilename())
+                    .tamanho(String.valueOf(file.getSize()))
+                    .build();
+
+            arquivosRepository.save(arquivo);
+
+        } catch (IOException e) {
+            throw new IllegalArgumentException("Não foi possivel salvar o arquivo \t :" + file.getOriginalFilename());
+        }
+
+    }
+}
