542440c5a5d90f14ecaef4f6ec3b1d46c42c12ab
MaxwellJalves <maxwell880@gmail.com>
2022-05-11 01:48:02 -0300

    Finalizado estudo do fluxo de obter um form-data e retornar um json no lado do backend, e entedido o fluxo que o front faz para mandar os dados de forma correta para o back

--- README.md
+++ README.md
@@ -13,3 +13,33 @@ conceito do git flow e realizando pequenos commits para cada etapada da criaçã
  
 
 >  https://www.atlassian.com/br/git/tutorials/comparing-workflows/gitflow-workflow
+
+
+## v2 Estudo --> utilização do Multipart/form-data
+
+Criação de endpoint que espera receber um arquivo e 2 atributos de um formulario e retorna os dados mockado com os dados obtidos.
+exemplo do formulário:
+
+     <form
+        encType="multipart/form-data"
+        method="post"
+        action="http://localhost:8083/v1/multipart">
+
+        <label for="file">Arquivo</label>
+        <input type="file" id="file" name="file"></input>
+
+        <label for="nome">Nome</label>
+        <input type="text" id="nome" name="nome"></input>
+
+        <label for="protocolo">protocolo</label>
+        <input type="text" id="protocolo" name="protocolo"></input>
+
+        <input type="submit"></input>
+      </form>
+
+
+- no exemplo criado foi verificado que caso só passe o id do campos como nome do atributo esperado pela apis
+  o back o atributo do input chegará como "NULL" para o back , e caso não tenha nenhuma tratativa irá retornar um
+  status code 500 pois o servidor não estava preparado tratar o nullpointException.
+ 
+- Após o mapeamento dos atributos realizado de forma correta o céu é o limite.
--- src/main/java/com/java/upload/controller/MultipartController.java
+++ src/main/java/com/java/upload/controller/MultipartController.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
@@ -13,19 +15,25 @@ import java.util.HashMap;
 
 @RestController
 @RequestMapping("v1")
+@Slf4j
 public class MultipartController {
 
-    @PostMapping("multipart")
-    public ResponseEntity<Object> salvarFormulario(@RequestParam("file") MultipartFile file, @ModelAttribute Usuario usuario) {
-        var size = file.getSize();
-        Long tamanho = size / 1024;
+    @PostMapping(value = "multipart", consumes = MediaType.MULTIPART_FORM_DATA_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> salvarFormulario(@RequestParam MultipartFile file, @ModelAttribute Usuario usuario) {
+
+        log.info(String.format("new request for resource /v1/multipart : solicitante = %s", usuario.getNome()));
+        return ResponseEntity.created(URI.create("upload")).body(mock(file, usuario));
+    }
+
+    private HashMap<String, Object> mock(MultipartFile file, Usuario usuario) {
         var map = new HashMap<String, Object>();
-        map.put("arquivo", file.getOriginalFilename());
-        map.put("size", file.getSize());
-        map.put("KB", tamanho.floatValue());
-        map.put("nome", usuario.getNome());
-        map.put("protocolo", usuario.getProtocolo());
-        return ResponseEntity.created(URI.create("upload")).body(map);
+        map.put("NOME_ARQUIVO", file.getOriginalFilename());
+        map.put("TAMANHO_ARQUIVO", file.getSize());
+        map.put("TAMANHO_KB", file.getSize() / 1024);
+        map.put("NOME_USUARIO", usuario.getNome());
+        map.put("PROTOCOLO_DA_REQUISICAO", usuario.getProtocolo());
+
+        return map;
     }
 }
 
